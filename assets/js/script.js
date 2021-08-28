55fdc6581c6b6632a4e931901e2e7989dfd4b116
roxanabangura <89669167+roxanabangura@users.noreply.github.com>
2021-08-28 12:14:37 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$('.testimonial-items').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
